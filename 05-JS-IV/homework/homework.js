6875338835a61ae44aee143536fc08b291ac1fdf
Maurololg <a1511250@adexperu.edu.pe>
2022-08-26 13:02:17 -0500

    first commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -155,7 +155,7 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-   producto.calcularPrecioDescuento = function (){
+   producto.calcularPrecioDescuento = function(){
     var descuento = producto.precio * producto.porcentajeDeDescuento
     var precioConDescuento = producto.precio - descuento
     return precioConDescuento;
