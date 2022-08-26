701fc181f5a1ea25c2fdad930994b03d8e183f5b
Maurololg <a1511250@adexperu.edu.pe>
2022-08-26 13:14:58 -0500

    Second commit

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -11,11 +11,11 @@ function crearUsuario() {
   function Usuario (opciones){
     this.usuario = opciones.usuario,
     this.nombre = opciones.nombre;
-    this.email = opciones.email
+    this.email = opciones.email;
     this.password = opciones.password
   };
   Usuario.prototype.saludar = function (){
-    return 'Hola, mi nombre es' + this.nombre};
+    return 'Hola, mi nombre es ' + this.nombre};
     return Usuario;
     
   }
